3c7586fa121c1c7eb4511705f3e5f93b53bd8d06
Chuanlon21 <chuanlon@buffalo.edu>
2024-04-19 15:11:08 -0400

    init

---
+++ PostDemo/PostDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostDemoApp.swift
+//  PostDemo
+//
+//  Created by Chuanlong Liu on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PostDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
